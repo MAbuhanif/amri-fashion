b0274bebc2a950c71f210f428ed0049db8d08cdb
MAbuhanif <mundinoabete@gmail.com>
2025-04-09 00:32:35 +0100

    Add initial media files and product categories fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True)
+
+    def __str__(self):
+        return self.name
