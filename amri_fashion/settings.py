8eea5b5191974fa04748338284375fc5866cfd0f
MAbuhanif <mundinoabete@gmail.com>
2025-04-08 20:14:27 +0100

    Add base CSS for homepage styling with background image and typography

--- amri_fashion/settings.py
+++ amri_fashion/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home', 
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -148,6 +148,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
