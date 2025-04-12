a5e03654724d4c8a81f9cf41b8351ec8320a4d8e
MAbuhanif <mundinoabete@gmail.com>
2025-04-12 17:01:59 +0100

    Add product detail view and template, update navigation links for products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -6,3 +6,10 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
     return render(request, 'products/products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    """ A view to show a single product detail page """
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/product_detail.html',
+                  {'product': product})
