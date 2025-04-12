84832aa591f430d4e05bf2eeb6683b9037d3a1ce
MAbuhanif <mundinoabete@gmail.com>
2025-04-12 21:16:52 +0100

    Add shopping bag context processor

--- amri_fashion/settings.py
+++ amri_fashion/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',  # Custom context processor 4 bag
             ],
         },
     },
@@ -170,3 +171,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_PERCENTAGE = 10  # Percentage for standard delivery
+FREE_DELIVERY_THRESHOLD = 50  # Threshold for free delivery
