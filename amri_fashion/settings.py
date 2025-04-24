57b052a60b2f7c755b9e3654aafe49999a549d01
MAbuhanif <mundinoabete@gmail.com>
2025-04-24 21:00:58 +0100

    Remove unused favicon files and references from base template

--- amri_fashion/settings.py
+++ amri_fashion/settings.py
@@ -95,7 +95,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_contents',  # Custom context processor for bag
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
