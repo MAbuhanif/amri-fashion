f9b5fcebfdc33d28467a88fd264b48ec3a819569
MAbuhanif <mundinoabete@gmail.com>
2025-04-25 21:04:28 +0100

    Enhance product navigation and detail views with category links and sorting options

--- products/views.py
+++ products/views.py
@@ -3,36 +3,73 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 from .forms import ProductForm
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    products = Product.objects.all().order_by('name')
+    products = Product.objects.all().order_by('category')
     query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request,
-                               "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
-            products = products.filter(queries)
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            if sortkey == 'name':
+                sort = 'name'
+                products = products.order_by(sort)
+            elif sortkey == 'category':
+                sort = 'category'
+                products = products.order_by(sort)
+            elif sortkey == 'price':
+                sort = 'price'
+                products = products.order_by(sort)
+            else:
+                sort = 'name'
+                products = products.order_by(sort)
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort = f'-{sort}'
+                    products = products.order_by(sort)
+                else:
+                    sort = f'{sort}'
+                    products = products.order_by(sort)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if request.GET:
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(
+                        request, "You didn't enter any search criteria!"
+                    )
+                    return redirect(reverse('products'))
+                queries = Q(name__icontains=query) | Q(
+                    description__icontains=query)
+                products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}' if sort else 'name_asc'
 
     paginator = Paginator(products, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'products/products.html',
-                  {
-                      'products': page_obj.object_list,
-                      'search_term': query,
-                      'page_obj': page_obj
-                  })
+    return render(request, 'products/products.html', {
+        'products': page_obj.object_list,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+        'page_obj': page_obj
+    })
 
 
 def product_detail(request, product_id):
