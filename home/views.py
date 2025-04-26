da6eb9bbbe1129230dada33fad5ddf60717e8d37
MAbuhanif <mundinoabete@gmail.com>
2025-04-26 21:50:53 +0100

    Add About Us page and update navigation links

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """View function for the home page of the site."""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """View function for the about page of the site."""
+    return render(request, 'home/about.html')
